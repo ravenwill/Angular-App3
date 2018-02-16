7bacec6572b2bdf148dabb5d6d64cc367a786dc2
ravenwill <will.alvesandrioli@gmail.com>
2018-02-16 17:47:34 -0200

    Criação da tela de login e inclusão das animações no banner e container de cadastro e login

---
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public exibirPainelLogin(): void {
+    this.exibirPainel.emit('login')
+  }
+
+}
---
+++ src/app/acesso/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public exibirPainelCadastro(): void {
+    this.exibirPainel.emit('cadastro')
+  }
+
+}
